61873cebf4e45120d1fdcf9a82f6a7876bb089e0
Erfan Ebrahimi <persionhost@gmail.com>
2021-11-19 13:13:41 +0330

    add php document for facade

--- src/Facades/DynamicForms.php
+++ src/Facades/DynamicForms.php
@@ -2,7 +2,33 @@
 namespace Yeganehha\DynamicForms\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use phpDocumentor\Reflection\Types\Integer;
 
+/**
+ * @method static DynamicForms form(Mixed $formName ,Mixed $model,Boolean $extend_table = false) Make new or import form.
+ * @method static boolean getTable() this form use external table or not.
+ * @method static DynamicForms setTable(Boolean $isExternal) set form use external table or not.
+ * @method static DynamicForms setExternalTable() set form use external table.
+ * @method static DynamicForms setLocalTable() set form use one table.
+ * @method static Mixed getFields(Boolean $isArray = false ,Boolean $showHidden = false) return fields of table.
+ * @method static DynamicForms setFields(array $fields = null) set fields of form.
+ * @method static DynamicForms addFields(array $field = null) add new field of form.
+ * @method static DynamicForms updateFields(Integer $fieldId , array $field = null) update field of form.
+ * @method static DynamicForms deleteFields(Integer $fieldId ) delete field of form.
+ * @method static Mixed getModel(Boolean $returnClass = false ) get model of form.
+ * @method static DynamicForms setFillOutData(array $data ) fill outed data of form.
+ * @method static DynamicForms fillOutForm(Mixed $model,Boolean $autoDetectData = true) fill outed data from request of form.
+ * @method static DynamicForms showHidden(Boolean $show = true) show hidden fields of form.
+ * @method static DynamicForms editForm() set form for edit fill outed form.
+ * @method static DynamicForms view() generate form for blade.
+ * @method static int|DynamicForms getId(Boolean $ViewVariable = false) get id of form.
+ * @method static Boolean getError() get form has error.
+ * @method static DynamicForms addFieldType(String $name ,String $label = null ,String $view = null) creat new field type.
+ * @method static DynamicForms removeFieldType(String $name ) remove new field type.
+ * @method static Boolean deleteForm() delete form.
+ * Class DynamicForms
+ * @package Yeganehha\DynamicForms\Facades
+ */
 class DynamicForms extends Facade
 {
 
